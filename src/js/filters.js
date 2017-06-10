777ee7e4450f2fdf88dc52cdae92f9bc42a6f838
Vinny <espinalv1@gmail.com>
2017-06-10 05:26:17 -0400

    Extracted filter logic and finished up filter functionality.

---
+++ src/js/filters.js
@@ -0,0 +1,68 @@
+'use strict'
+
+var twitchFCC = window.twitchFCC = window.twitchFCC || {}
+
+twitchFCC.filters = {
+  init () {
+    const filters = document.querySelector('.filters');
+
+    filters.addEventListener('click', this.filtersClickListener.bind(this));
+  },
+
+  filtersClickListener (e) {
+    const targetID = e.target.id;
+    const nodeName = e.target.nodeName.toLowerCase()
+
+    if (nodeName === 'span') {
+      this.setActiveFilter(targetID)
+      this.filterList(targetID)
+    }
+  },
+
+  setActiveFilter (nextActiveFilter) {
+    const activeFilter = document.querySelector('.active')
+    const targetFilter = document.querySelector(`#${nextActiveFilter}`)
+    
+    if (activeFilter.classList.contains(nextActiveFilter)) {
+      return
+    }
+
+    activeFilter.classList.remove('active')
+    targetFilter.classList.add('active')
+  },
+
+  filterList (targetID) {
+    const filterType = targetID.split('--')[1]
+    const streams = document.querySelectorAll('.streams_container--item')
+
+    switch (filterType) {
+      case 'all':
+        streams.forEach(node => {
+          if (node.classList.contains('hide')) {
+            node.classList.remove('hide')
+          }
+        })
+        break
+      case 'online':
+         streams.forEach(node => {
+          if (node.classList.contains('offline')) {
+            node.classList.add('hide')
+          } else if (node.classList.contains('online')) {
+            node.classList.remove('hide')
+          }
+        })
+        break
+      case 'offline':
+        streams.forEach(node => {
+          if (node.classList.contains('online')) {
+            node.classList.add('hide')
+          } else if (node.classList.contains('offline')) {
+            node.classList.remove('hide')
+          }
+        })
+        break
+      default:
+        return
+    }
+  }
+}
--- src/js/index.js
+++ src/js/index.js
@@ -5,10 +5,10 @@ var twitchFCC = window.twitchFCC = window.twitchFCC || {}
 window.addEventListener('load', e => {
   twitchFCC.streamListing.init()
   twitchFCC.fetchApi.init()
+  twitchFCC.filters.init()
 
   PubSub.subscribe('API_DATA_RECEIVED', apiDataReceived)
   PubSub.subscribe('FORMATTED_DATA_RECEIVED', formattedDataReceived)
-  bindFilterListener();
   
   PubSub.publish('GET_API_DATA', ['freecodecamp', 'ESL_SC2', 'OgamingSC2', 'storbeck', 'noobs2ninjas', 'RobotCaleb'])
 
@@ -25,24 +25,4 @@ const formattedDataReceived = (event, formattedData) => {
   }
 
   PubSub.publish('APPEND_DATA', dataObj)
-}
-
-const bindFilterListener = () => {
-  const filters = document.querySelector('.filters');
-
-  filters.addEventListener('click', (e) => {
-    const targetClass = e.target.className;
-    const nodeName = e.target.nodeName.toLowerCase();
-    
-    if (nodeName === 'span') {
-      setActiveFilter(targetClass, filters);
-    }
-  });
-}
-
-const setActiveFilter = (nextActiveFilter, parentEl) => {
-  const activeFilter = parentEl.querySelector('.active');
-  const targetFilter = parentEl.querySelector(`.${nextActiveFilter}`);
-  activeFilter.classList.remove('active');
-  targetFilter.classList.add('active');
 }
